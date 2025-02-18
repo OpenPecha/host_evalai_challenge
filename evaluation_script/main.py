87b06a28e84670260e2d6392cc2be0b1a756aff9
Tashi <tashidhondup55@gmail.com>
2025-02-18 12:16:52 +0530

    due to error reverting to see any changes in error

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -1,65 +1,65 @@
 import json
-from evaluate import load
+# from evaluate import load
 
-cer_metric = load("cer")
-wer_metric = load("wer")
+# cer_metric = load("cer")
+# wer_metric = load("wer")
 
-def calculate_cer(true_text, inference_text):
-    try:
-        cer = cer_metric.compute(references=true_text, predictions=inference_text)
-        cer = min(cer, 1.0)
-        return cer
-    except:
-        return 0.0
+# def calculate_cer(true_text, inference_text):
+#     try:
+#         cer = cer_metric.compute(references=true_text, predictions=inference_text)
+#         cer = min(cer, 1.0)
+#         return cer
+#     except:
+#         return 0.0
     
 
-def calculate_wer(true_text, inference_text):
-    try:
-        wer = wer_metric.compute(references=true_text, predictions=inference_text)
-        wer = min(wer, 1.0)
-        return wer
-    except:
-        return 0.0
+# def calculate_wer(true_text, inference_text):
+#     try:
+#         wer = wer_metric.compute(references=true_text, predictions=inference_text)
+#         wer = min(wer, 1.0)
+#         return wer
+#     except:
+#         return 0.0
 
 
-# def calculate_edit_distance(reference, hypothesis):
-#     """
-#     Compute the edit distance (Levenshtein distance) between reference and hypothesis.
-#     """
-#     m, n = len(reference), len(hypothesis)
-#     dp = [[0] * (n + 1) for _ in range(m + 1)]
+def calculate_edit_distance(reference, hypothesis):
+    """
+    Compute the edit distance (Levenshtein distance) between reference and hypothesis.
+    """
+    m, n = len(reference), len(hypothesis)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-#     for i in range(m + 1):
-#         for j in range(n + 1):
-#             if i == 0:
-#                 dp[i][j] = j  # All insertions
-#             elif j == 0:
-#                 dp[i][j] = i  # All deletions
-#             elif reference[i - 1] == hypothesis[j - 1]:
-#                 dp[i][j] = dp[i - 1][j - 1]  # No change
-#             else:
-#                 dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
-#     return dp[m][n]
-
-
-# def calculate_cer(reference, hypothesis):
-#     """
-#     Calculate Character Error Rate (CER).
-#     """
-#     reference = reference.replace(" ", "")  # Remove spaces for CER
-#     hypothesis = hypothesis.replace(" ", "")  # Remove spaces for CER
-#     distance = calculate_edit_distance(reference, hypothesis)
-#     return distance / len(reference) if len(reference) > 0 else 0
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                dp[i][j] = j  # All insertions
+            elif j == 0:
+                dp[i][j] = i  # All deletions
+            elif reference[i - 1] == hypothesis[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]  # No change
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+    return dp[m][n]
 
 
-# def calculate_wer(reference, hypothesis):
-#     """
-#     Calculate Word Error Rate (WER).
-#     """
-#     ref_words = reference.split()
-#     hyp_words = hypothesis.split()
-#     distance = calculate_edit_distance(ref_words, hyp_words)
-#     return distance / len(ref_words) if len(ref_words) > 0 else 0
+def calculate_cer(reference, hypothesis):
+    """
+    Calculate Character Error Rate (CER).
+    """
+    reference = reference.replace(" ", "")  # Remove spaces for CER
+    hypothesis = hypothesis.replace(" ", "")  # Remove spaces for CER
+    distance = calculate_edit_distance(reference, hypothesis)
+    return distance / len(reference) if len(reference) > 0 else 0
+
+
+def calculate_wer(reference, hypothesis):
+    """
+    Calculate Word Error Rate (WER).
+    """
+    ref_words = reference.split()
+    hyp_words = hypothesis.split()
+    distance = calculate_edit_distance(ref_words, hyp_words)
+    return distance / len(ref_words) if len(ref_words) > 0 else 0
 
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
